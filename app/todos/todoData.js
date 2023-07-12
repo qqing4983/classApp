31046b0974a6cee19fb173d664e554e0ba9fa49c
qqing4983 <qqing4983@gmail.com>
2023-07-12 17:08:14 +0800

    delete

--- app/todos/todoData.js
+++
@@ -1,6 +0,0 @@
-const fetchTodos = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/todos/")
-    const todos = await res.json();
-    return todos;
-}
-export const todos = await fetchTodos();
